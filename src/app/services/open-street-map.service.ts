479704d497bb5617ad8e0d63e9b2230f3941b7f1
nanda-03 <56308379+nanda-03@users.noreply.github.com>
2019-12-30 12:32:17 +0100

    Ajout du service services/OpenStreetMap

--- src/app/services/open-street-map.service.ts
+++ src/app/services/open-street-map.service.ts
@@ -3,7 +3,35 @@ import { Injectable } from '@angular/core';
 @Injectable({
   providedIn: 'root'
 })
+/** @description Le service de map depend de Leaflet qui doit être inclure à notre page dans l'entête
+ * Raison pour laquelle plus bas vous croirez qu'il y'a une erreur avec "L". Le code à inserer dans l'entete est dispoible chez Nanda.
+ */
 export class OpenStreetMapService {
 
-  constructor() { }
+    latitude=4.04827;
+    longitude=9.70428; //Il s'agit des coordonnées de Douala
+    macarte;
+  constructor() {
+    
+   }
+   /** @description Initialise la map:permet d'afficher la map centrer en latitude et longitude.
+   */
+  initMap() {       
+		this.macarte = L.map('map').setView([this.latitude, this.longitude], 11);
+          
+		L.tileLayer('https://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png', {attribution: 'données © <a href="//osm.org/copyright">OpenStreetMap</a>/ODbL - rendu <a href="//openstreetmap.fr">OSM France</a>',
+                    minZoom: 1,
+                    maxZoom: 20
+                }).addTo(this.macarte);
+           var marker = L.marker([this.latitude, this.longitude]).addTo(this.macarte);
+ }
+ /** @description Recupere les coordonnées geographique du lieu choisi par l'utilisateur.
+ * @return {LatLng}
+ * Utiliser ".lat" pour recuperer la latitude et ".lng" pour la logitude
+ */
+ getlocation(){
+    this.latitude=this.macarte.getCenter().lat;
+    this.longitude=this.macarte.getCenter().lng;
+    return this.macarte.getCenter();
+ }
 }
